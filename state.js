aef4b7e078631d6b2b3b1437bda91f3cd7abc235
Haruto Tsutsui <harufelis@users.noreply.github.com>
2026-04-22 21:07:44 +0900

    state.jsの中身を作成しました

--- state.js
+++ state.js
@@ -1,1 +1,27 @@
+export const gameState = {
+  board: ['', '', '', '', '', '', '', '', ''],
+  currentPlayer: 'X',
+  isGameActive: true,
 
+  updateBoard(index) {
+    if (this.board[index] === '' && this.isGameActive) {
+      this.board[index] = this.currentPlayer;
+      return true;
+    }
+    return false;
+  },
+
+  switchPlayer() {
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  },
+
+  endGame() {
+    this.isGameActive = false;
+  },
+
+  reset() {
+    this.board.fill('');
+    this.currentPlayer = 'X';
+    this.isGameActive = true;
+  },
+};
