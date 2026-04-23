d8828cecdc5bd7e6ba184676c1d412ce7726de7d
erisa <tnkwkn782@gmaile.com>
2026-04-23 21:34:23 +0900

    ui.jsのupdateCellUIとresetUIを作りました

--- ui.js
+++ ui.js
@@ -1,1 +1,18 @@
+//クリックされたマスに"X" or "O"を表示し、色を変える関数
+export function updateCellUI(cell, player) {
+    cell.textContent = player;
+    
+    if (player === 'X') {
+        cell.style.color = 'red';
+    } else if (player === 'O') {
+        cell.style.color = 'blue';
+    }
+}
 
+// マス目UIのリセット
+export function resetUI(cells) {
+    cells.forEach(cell => cell.textContent = '');
+// 結果画面UIのリセット
+    const resultPage = document.getElementById("resultPage");
+    resultPage.innerHTML = "";
+}
